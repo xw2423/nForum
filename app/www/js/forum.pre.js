abbdb2126e0ab72c3d9b1d0cd8f5b1a1dccf11ae
xw2423 <wei.xiao.bupt@gmail.com>
2012-02-25 22:29:35 +0800

    modify the way that jump to other web in forum.pre.js

--- app/www/js/forum.pre.js
+++ app/www/js/forum.pre.js
@@ -1,11 +1,11 @@
-var login = ["user/ajax_login"],
-home = ['/#!' + SYS.home.substr(1)];
+var login = [SYS.base + '/' + SYS.ajax.login],
+home = [SYS.base + '/#!' + SYS.home.substr(1)];
 $(function(){
     $('#id').focus();
     $('#f_login').submit(function(){
         var v = $('#s-mode').val();
         if(v == '0'){
-            $.post(SYS.base + '/' + SYS.ajax.login, $(this).getPostData(), function(json){
+            $.post(login[v], $(this).getPostData(), function(json){
                 if(json.ajax_st == 1)
                     window.location.href = SYS.base + '/';
                 else
@@ -14,10 +14,10 @@ $(function(){
             }, 'json');
             return false;
         }
-        $(this).attr('action', SYS.base + login[v]);
+        $(this).attr('action', login[v]);
     });
     $('#b_guest').click(function(){
-        window.location.href = SYS.base + home[$('#s-mode').val()]
+        window.location.href = home[$('#s-mode').val()]
     });
     $('#b_reg').click(function(){
         window.location.href = SYS.base + "/#!reg";
